4ac933074d510f81c3343d3dd11ad8f5f83e18a2
Sam Marxz <sammarxzz@gmail.com>
2021-01-20 10:58:00 -0300

    hotfix;

--- script.py
+++ script.py
@@ -11,6 +11,6 @@ session = InstaPy(username=USERNAME, password=PASSWORD)
 
 
 with smart_run(session):
-    session.follow_user_followers(["frontendjoe", amount=3, randomize=False])
+    session.follow_user_followers(["frontendjoe"], amount=3, randomize=False)
 
     session.join_pods()
